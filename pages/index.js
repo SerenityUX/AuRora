06158554da00a0835af4401fd4e1e4a6aa309661
SerenityUX <thomas@serenidad.app>
2023-04-01 12:54:56 -0400

    added selection

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,197 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [step, setStep] = useState(0);
-  const [phone, setPhone] = useState("");
+  const [selectedClothes, setSelectedClothes] = useState([]);
 
+  const [phone, setPhone] = useState("");
+  const clothingOptions = [
+    {
+      "name": "T-Shirt",
+      "type": "Top",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "Polo Shirt",
+      "type": "Top",
+      "styles": ["Casual", "Athletic", "Professional"],
+      "temperatureType": ["Cool", "Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "Sweater",
+      "type": "Top",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Hoodie",
+      "type": "Top",
+      "styles": ["Athletic", "Streetwear"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Blouse",
+      "type": "Top",
+      "styles": ["Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Tank Top",
+      "type": "Top",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "Button-Up Shirt",
+      "type": "Top",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Sweatshirt",
+      "type": "Top",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Cardigan",
+      "type": "Top",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Turtleneck",
+      "type": "Top",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Jacket",
+      "type": "Outerwear",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Parka",
+      "type": "Outerwear",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Raincoat",
+      "type": "Outerwear",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Peacoat",
+      "type": "Outerwear",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Bomber Jacket",
+      "type": "Outerwear",
+      "styles": ["Athletic", "Streetwear"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Blazer",
+      "type": "Outerwear",
+      "styles": ["Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Leather Jacket",
+      "type": "Outerwear",
+      "styles": ["Casual", "Streetwear"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Denim Jacket",
+      "type": "Outerwear",
+      "styles": ["Casual", "Streetwear"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Vest",
+      "type": "Outerwear",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Jeans",
+      "type": "Bottoms",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Khaki Pants",
+      "type": "Bottoms",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Sweatpants",
+      "type": "Bottoms",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Leggings",
+      "type": "Bottoms",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Shorts",
+      "type": "Bottoms",
+      "styles": ["Casual", "Athletic"],
+      "temperatureType": ["Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "Skirt",
+      "type": "Bottoms",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Dress",
+      "type": "Dresses",
+      "styles": ["Casual", "Professional", "Formal"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Sneakers",
+      "type": "Shoes",
+      "styles": ["Athletic", "Casual"],
+      "temperatureType": ["Cool", "Warm"]
+    },
+    {
+      "name": "Boots",
+      "type": "Shoes",
+      "styles": ["Casual", "Professional"],
+      "temperatureType": ["Cool", "Freezing", "Extremely Cold and Dangerous"]
+    },
+    {
+      "name": "Flip Flops",
+      "type": "Shoes",
+      "styles": ["Casual", "Beachwear"],
+      "temperatureType": ["Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "Sandals",
+      "type": "Shoes",
+      "styles": ["Casual", "Beachwear"],
+      "temperatureType": ["Warm", "Hot", "Extremely Hot and Dangerous"]
+    },
+    {
+      "name": "High Heels",
+      "type": "Shoes",
+      "styles": ["Professional", "Formal"],
+      "temperatureType": ["Cool", "Warm"]
+    }
+  ] 
   const weatherRanges = [
     {
       "type": "Extremely Cold and Dangerous",
@@ -100,7 +289,21 @@ export default function Home() {
         {step == 1 ? (
           <div>
             <h1>Select Clothes You Like</h1>
-
+            {clothingOptions.map((option) => 
+            <div onClick={() => {
+              if (!selectedClothes.includes(option.name)) {
+                setSelectedClothes([...selectedClothes, option.name])
+              } else {
+                console.log("ok")
+                setSelectedClothes(oldValues => {
+                  return oldValues.filter(optionChoice => optionChoice.name !== option.name)
+                })
+              }
+              console.log(selectedClothes)
+            }}>
+              <p>{option.name}</p>
+            </div>
+            )}
           </div>
         ) : 
         (
@@ -120,7 +323,7 @@ export default function Home() {
         {step == 3 ? (
           <div>
             <p>Phone Number</p>
-            
+
           </div>
         ) : 
         (
