34c96e49901b89823719147b11461c3e8de330a0
SerenityUX <thomas@serenidad.app>
2023-04-01 17:13:35 -0400

    added name

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [successful, setSuccessful] = useState(false);
+  const [name, setName] = useState("");
 
-  const [step, setStep] = useState(0);
   const [selectedClothes, setSelectedClothes] = useState([]);
   const [zipCode, setZipCode] = useState("");
   const [zipCodeState, setZipCodeState] = useState("");
@@ -22,6 +22,34 @@ export default function Home() {
 
   const [phone, setPhone] = useState("");
   var zipcodes = require('zipcodes');
+
+  const handleSubmit = async () => {
+
+    try {
+      const response = await fetch('https://2068-38-122-127-226.ngrok.io/api/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          user: {
+            zip_code: zipCode,
+            phone_number: phone,
+            first_name: name
+          }
+        })
+      });
+      if (!response.ok) {
+        
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <>
@@ -31,25 +59,32 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-          
-
-              <main style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw", margin: -8}}>
-              <div style={{backgroundColor: "#fff", width: "fit-content", padding: 8, borderRadius: 16, margin: "16"}}>
-                <div>
-                  <h1 style={{margin: 0, fontSize: 36}}>Aurora</h1>
-                  <p style={{fontSize: 18}}>Morning mobile notifications to let you know what<br/> clothes to wear based on the weather in your zip code</p>
-                  {successful ? (                  <Confetti
+      {successful ? (                  <Confetti
                   style={{height: "100vh", width: "100vw"}}
       width={"1000px"}
       height={"1000px"}
     />) : 
                   (null)}
 
+              <main style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw", margin: -8}}>
+              {!successful ? (
+              <div style={{backgroundColor: "#fff", width: "fit-content", padding: 8, borderRadius: 16, margin: 16}}>
+
+                <div>
+                  <h1 style={{margin: 0, fontSize: 36}}>Aurora</h1>
+                  <p style={{fontSize: 18, maxWidth: 512}}>Morning mobile notifications to let you know what clothes to wear based on the weather in your zip code</p>
+
+
                 </div>
           
+              <div style={{marginBottom: 16}}>
+                <p style={{fontSize: 14, margin: 0}}>First Name</p>
+                <input type="first" placeholder="Marsha" value={name} onChange={event => setName(event.target.value)}/>
+              </div>
             <div style={{marginBottom: 16}}>
+              
               <p style={{fontSize: 14, margin: 0}}>Zipcode</p>
-              <input type="zipcode" placeholder="Zip Code" value={zipCode} onChange={event => {
+              <input type="zipcode" placeholder="29135" value={zipCode} onChange={event => {
                 setZipCode(event.target.value)
                 if(zipcodes?.lookup(event.target.value) != undefined) {
                   setZipCodeState(zipcodes?.lookup(event.target.value))
@@ -70,17 +105,39 @@ export default function Home() {
                 defaultCountry="US"
                 onChange={setPhone}/>
               <button 
+                  disabled={!(phone != "" && zipCode != "" && name != "")}
                   onClick={() => {
-                      console.log({user: {"zip": zipCode, "phone_number": phone}})
+                      handleSubmit()
                       setSuccessful(true)
                   }}
-                  style={{backgroundColor: "#258CD6", color: "#fff", padding: "10px 20px", marginTop: 16, width: "100%", border: "none", borderRadius: "5px", cursor: "pointer"}}
+                  style={{backgroundColor: "#258CD6", opacity: phone != "" && zipCode != "" && name != "" ? (1) : (0.5), color: "#fff", padding: "10px 20px", marginTop: 16, width: "100%", border: "none", borderRadius: "5px", cursor: "pointer"}}
               >
                   Submit
               </button>
           </div>
-          <h1></h1>
-        </div>
+        </div>) : (
+          <div style={{backgroundColor: "#fff", maxWidth: 612, width: "fit-content", padding: 16, borderRadius: 16, margin: 16}}>
+             <p> 
+              Dear beloved Hack Clubber,
+On behalf of the admissions committee at Aurora, it is with great pleasure that I extend our sincerest congratulations on your acceptance to our exclusive fashion technology program. We were impressed by your passion for innovation and your eagerness to improve your daily routine with the help of cutting-edge technology.
+<br/>
+Your enrollment in Aurora will grant you access to our state-of-the-art application, which will provide you with daily outfit recommendations tailored to your unique style preferences and weather conditions. We are confident that this innovative tool will enhance your day-to-day life and simplify your morning routine.
+<br/>
+
+We believe that you will thrive in our dynamic community of like-minded individuals who are dedicated to utilizing technology to solve real-world problems. We look forward to supporting you as you embark on this exciting journey with us.
+<br/>
+
+Congratulations again on your acceptance to Aurora. We are thrilled to welcome you to our program and can't wait to see the great things you will achieve with our application.
+<br/>
+
+Sincerely,
+<br/>
+
+Thomas,
+
+Aurora Admissions Committee</p>
+          </div>
+          )}
       </main>
     </>
   )
