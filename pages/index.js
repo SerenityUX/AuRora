2a6e18c6031f0be2bd16a95362d10305b6b93dc1
SerenityUX <thomas@serenidad.app>
2023-04-01 17:58:18 -0400

    change URL

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
   const handleSubmit = async () => {
 
     try {
-      const response = await fetch('https://2068-38-122-127-226.ngrok.io/api/users/', {
+      const response = await fetch('https://b9ca-38-122-127-226.ngrok.io/api/users/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
